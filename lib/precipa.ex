556ac976e666b173a069fd14a652ef498376622e
Gordon Guthrie <gg104o@alt01841.corp.wayfair.com>
2020-01-26 23:03:51 +0100

    restructure code

---
+++ lib/precipa.ex
@@ -0,0 +1,58 @@
+defmodule Precipa do
+
+  use LexLuthor
+
+  @moduledoc """
+  Documentation for Radikigo.
+  """
+
+  @doc """
+  radikigo
+
+  """
+
+  # divide text
+  def dividu_teksto(teksto) when is_binary(teksto) do
+    IO.inspect(teksto)
+  end
+
+  # analyse text
+  def analazistu_teksto(teksto) when is_binary(teksto) do
+    tokenoj = lexu(teksto)
+    frazoj = amasigu_frazo(tokenoj)
+  end
+
+  # lex
+  def lexu(teksto) when is_binary(teksto) do
+    {:ok, tokenoj} = Lexo.lex(teksto)
+    tokenoj
+    |> amasigu_tokenoj
+  end
+
+  # collect tokens
+  defp amasigu_tokenoj(tokenoj) do
+    for %LexLuthor.Token{name: n, value: v} <- tokenoj, n != :ws, do: {n, v}
+  end
+
+  # collect sentences (the parser)
+  defp amasigu_frazo(tokenoj) do
+    amasigu_frazo2(tokenoj, false, [], [])
+  end
+
+  defp amasigu_frazo2([], _is_in_quotes, [], frazoj), do: Enum.reverse(frazoj)
+  defp amasigu_frazo2([], _is_in_quotes, frazo, frazoj), do: Enum.reverse([Enum.reverse(frazo) | frazoj])
+  defp amasigu_frazo2([{:quotes, q} | tail], false, frazo, frazoj) do
+    amasigu_frazo2(tail, true, [{:quotes, q} | frazo], frazoj)
+  end
+  defp amasigu_frazo2([{:quotes, q} | tail], true, frazo, frazoj) do
+    amasigu_frazo2(tail, false, [{:quotes, q} | frazo], frazoj)
+  end
+  defp amasigu_frazo2([{:punkto, p} | tail], false, frazo, frazoj) do
+    neufrazo = Enum.reverse([{:punkto, p} | frazo])
+    amasigu_frazo2(tail, false, [], [neufrazo | frazoj])
+  end
+  defp amasigu_frazo2([head | tail], is_in_quotes, frazo, frazoj) do
+    amasigu_frazo2(tail, is_in_quotes, [head | frazo], frazoj)
+  end
+
+end
---
+++ test/precipa_test.exs
@@ -0,0 +1,114 @@
+defmodule PrecipaTest do
+  use ExUnit.Case
+  doctest Precipa
+
+  test "lex simple words" do
+
+    teksto = "laŭ Ludoviko Zamenhof bongustas freŝa ĉeĥa manĝaĵo kun spicoj. LAŬ LUDOVIKO ZAMENHOF BONGUSTAS FREŜA ĈEĤA MANĜAĴO KUN SPICOJ"
+
+    lexigoj = [
+      vorto:  "laŭ",
+      vorto:  "Ludoviko",
+      vorto:  "Zamenhof",
+      vorto:  "bongustas",
+      vorto:  "freŝa",
+      vorto:  "ĉeĥa",
+      vorto:  "manĝaĵo",
+      vorto:  "kun",
+      vorto:  "spicoj",
+      punkto: ".",
+      vorto:  "LAŬ",
+      vorto:  "LUDOVIKO",
+      vorto:  "ZAMENHOF",
+      vorto:  "BONGUSTAS",
+      vorto:  "FREŜA",
+      vorto:  "ĈEĤA",
+      vorto:  "MANĜAĴO",
+      vorto:  "KUN",
+      vorto:  "SPICOJ"
+    ]
+
+    assert Precipa.lexu(teksto) == lexigoj
+
+  end
+
+  test "parse simple sentences" do
+
+    teksto = "laŭ Ludoviko Zamenhof bongustas freŝa ĉeĥa manĝaĵo kun spicoj. LAŬ LUDOVIKO ZAMENHOF BONGUSTAS FREŜA ĈEĤA MANĜAĴO KUN SPICOJ"
+
+    frazoj = [
+      [
+        vorto:  "laŭ",
+        vorto:  "Ludoviko",
+        vorto:  "Zamenhof",
+        vorto:  "bongustas",
+        vorto:  "freŝa",
+        vorto:  "ĉeĥa",
+        vorto:  "manĝaĵo",
+        vorto:  "kun",
+        vorto:  "spicoj",
+        punkto: "."
+      ],
+      [
+        vorto:  "LAŬ",
+        vorto:  "LUDOVIKO",
+        vorto:  "ZAMENHOF",
+        vorto:  "BONGUSTAS",
+        vorto:  "FREŜA",
+        vorto:  "ĈEĤA",
+        vorto:  "MANĜAĴO",
+        vorto:  "KUN",
+        vorto:  "SPICOJ"
+      ]
+    ]
+
+    assert Precipa.analazistu_teksto(teksto) == frazoj
+
+  end
+
+  test "parse quotes" do
+
+    teksto = "aa bb \"cc dd. ee\" ff. gg"
+
+    frazoj = [
+      [
+        vorto:  "aa",
+        vorto:  "bb",
+        quotes:  "\"",
+        vorto:  "cc",
+        vorto:  "dd",
+        punkto: ".",
+        vorto:  "ee",
+        quotes:  "\"",
+        vorto:  "ff",
+        punkto: "."
+      ],
+      [
+        vorto:  "gg",
+      ]
+    ]
+
+    assert Precipa.analazistu_teksto(teksto) == frazoj
+
+  end
+
+
+  test "break out sentences" do
+
+    teksto = "La senpotencon pruvas la problemo de la medio. Kvar jarojn post la solenaj proklamoj de la COP 21, la lako jam defalis. La planedo de riĉuloj ne malaltigis siajn apetitojn de konsumado ; la riskoj de tro-varmiĝo preciziĝis. La socialista urbestrino de Parizo s-ino Anne Hidalgo, faras unu ekologian predikon post la alia, sed ŝi lasas la grandajn konstruaĵojn de la ĉefurbo briligi la gigantajn reklamlumojn de luksaj markoj aŭ de poŝtelefonoj. Kaj la franca ministro pri transporto ĝuas la promesplenajn karierojn en la sektoro de sia respondeco : “Ni bezonas tridek mil ŝoforojn en la venontaj jaroj, do tio estas metio, kiun oni devas valorigi, speciale ĉe junuloj.” Pli da ŝoforoj sur la stratoj, pli da “Macron-veturiloj”, jen kio protektas la ekologian sistemon. La fervoja vartransporto, la fervojo ? Ekster la demando, ĉar necesas batali kontraŭ la troa personaro en la publikaj entreprenoj."
+
+    frazoj = [
+      "La senpotencon pruvas la problemo de la medio.",
+      "Kvar jarojn post la solenaj proklamoj de la COP 21, la lako jam defalis.",
+      "La planedo de riĉuloj ne malaltigis siajn apetitojn de konsumado ; la riskoj de tro-varmiĝo preciziĝis.",
+      "La socialista urbestrino de Parizo s-ino Anne Hidalgo, faras unu ekologian predikon post la alia, sed ŝi lasas la grandajn konstruaĵojn de la ĉefurbo briligi la gigantajn reklamlumojn de luksaj markoj aŭ de poŝtelefonoj.",
+      "Kaj la franca ministro pri transporto ĝuas la promesplenajn karierojn en la sektoro de sia respondeco : “Ni bezonas tridek mil ŝoforojn en la venontaj jaroj, do tio estas metio, kiun oni devas valorigi, speciale ĉe junuloj.”",
+      "Pli da ŝoforoj sur la stratoj, pli da “Macron-veturiloj”, jen kio protektas la ekologian sistemon.",
+      "La fervoja vartransporto, la fervojo ?",
+      "Ekster la demando, ĉar necesas batali kontraŭ la troa personaro en la publikaj entreprenoj."
+    ]
+    assert Precipa.dividu_teksto(teksto) == frazoj
+
+  end
+
+end
